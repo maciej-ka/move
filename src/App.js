6b48cd64319bb63ca71db6575b178a5cabad5a6e
maciej-ka <maciej.kasprzyk.it@gmail.com>
2017-08-22 21:43:39 +0200

    spike: drag and drop in svg

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  select(event, id) {
+    const {clientX, clientY} = event
+    this.setState({
+      ...this.state,
+      selection: {
+        x: this.state.data[id].x,
+        y: this.state.data[id].y,
+        clientX,
+        clientY,
+        id
+      }
+    })
+  }
+
+  move(event) {
+    const {selection, data} = this.state
+    if(!selection) return
+    const {clientX, clientY} = event
+    const selected = data[selection.id]
+    selected.x = selection.x - selection.clientX + clientX
+    selected.y = selection.y - selection.clientY + clientY
+    this.setState({
+      ...this.state,
+      data
+    })
+  }
+
+  deselect() {
+    this.setState({
+      ...this.state,
+      selection: null
+    })
+  }
+
+  constructor() {
+    super()
+    this.state = {
+      selection: null,
+      data: {
+        1: {
+          id: 1,
+          name: 'Object A',
+          fields: [
+            'field 1',
+            'field 2'
+          ],
+          x: 20,
+          y: 20
+        },
+        2: {
+          id: 2,
+          name: 'Object B',
+          fields: [
+            'field 1',
+            'field 2'
+          ],
+          x: 20,
+          y: 120
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <svg ref='svg' onMouseUp={() => this.deselect()} onMouseMove={event => this.move(event)}>
+          {Object.values(this.state.data).map(data =>
+            <rect onMouseDown={event => this.select(event, data.id)} key={data.id} x={data.x} y={data.y} width='80' height='80' style={{fill: 'hsla(0, 0%, 0%, 0.5)'}} />
+          )}
+        </svg>
+        <div>
+          {JSON.stringify(this.state)}
+        </div>
+      </div>
+    )
+  }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <div className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h2>Welcome to React</h2>
+  //       </div>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
+}
+
+export default App
